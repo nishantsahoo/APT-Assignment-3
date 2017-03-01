d4a4a2b516ee531bf5a4af4cd89607344497d78c
Nishant Sahoo <nishantsahoo.in@gmail.com>
2017-03-01 20:05:19 +0530

    Changes to init.py

--- init.py
+++ init.py
@@ -1,4 +1,7 @@
 import os
 print("1. Q1  \n2. Q2  \n3. Q3  \n4. Q4  \n5. Q5\n")
 choice = int(input("Enter the desired choice: "))
-os.system("python Q" + str(choice) + ".py")
+if choice not in range(1,6):
+    print("Wrong choice")
+else:
+    os.system("python Q" + str(choice) + ".py")
