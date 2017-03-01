664c9377515b7f008dcc408235ca696caf9b6b0b
Nishant Sahoo <nishantsahoo.in@gmail.com>
2017-03-01 20:15:00 +0530

    Added solution to Q2

---
+++ Q2.py
@@ -0,0 +1,15 @@
+l1 = input("Enter the first expression: ")
+l2 = input("Enter the second expression: ")
+l1_list = l1.split('+')
+l2_list = l2.split('+')
+result = []
+for i in range(0, 3):
+    l1_list[i] = l1_list[i][:-1]
+for i in range(0, 3):
+    l2_list[i] = l1_list[i][:-1]
+for i in range(0, 3):
+    result.append(str((int(l1_list[i])+int(l2_list[i]))))
+for i in range(0, 3):
+    result[i] += chr((ord("x")+i))
+answer = "+".join(result)
+print(answer)
--- init.py
+++ init.py
@@ -1,7 +1,7 @@
 import os
 print("1. Q1  \n2. Q2  \n3. Q3  \n4. Q4  \n5. Q5\n")
 choice = int(input("Enter the desired choice: "))
-if choice not in range(1,6):
+if choice not in range(1, 6):
     print("Wrong choice")
 else:
     os.system("python Q" + str(choice) + ".py")
