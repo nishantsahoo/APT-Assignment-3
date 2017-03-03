25330e0206cdbe8147ad2b0e0ecb7407a0df7ddb
Nishant Sahoo <nishantsahoo.in@gmail.com>
2017-03-03 12:23:08 +0530

    Added solution to Q2

---
+++ Q1.py
@@ -0,0 +1,43 @@
+import collections
+import random
+indices = list(range(0, 100))
+userDictionary = {}  # empty dictionary
+intCount = 0
+stringCount = 0
+counter = 0
+while 1:
+    value = input('Enter a value: ')
+    ans = input('Want to enter another value? y/n: ')
+    key = random.choice(indices)
+    userDictionary.update({key: value})
+    indices.remove(key)
+    counter += 1
+    if ans != "y":
+        break
+
+print(userDictionary)
+
+sum = 0
+concString = ""
+
+if intCount == counter:
+    for key, value in userDictionary.items():
+        sum += value
+    print("Sum of values: " + sum)
+
+if stringCount == counter:
+    for key, value in userDictionary.items():
+        concString += value
+    print("Concatenated string: " + concString)
+
+searchValue = input("Enter the string to be searched: ")
+found = 0
+for key, value in userDictionary.items():
+    if value == searchValue:
+        print("Found: " + str((key, value)))
+        found = 1
+        break
+
+if not found:
+    print("Not found")
+
